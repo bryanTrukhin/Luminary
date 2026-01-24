0ab37f3c5dd20b8a4bb6966f895dd0e108455535
Nam <namhieudang2008@gmail.com>
2026-01-24 16:46:19 -0500

    Node for a*

---
+++ Assets/Scripts/NavNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NavNode
+{
+    public int clusterIndex;
+    public Vector3 center;
+    public Bounds bounds;
+
+    public float gCost;
+    public float hCost;
+    public float fCost => gCost + hCost;
+    public NavNode parent;
+
+    public NavNode(int index, Vector3 center, Bounds bounds)
+    {
+        this.clusterIndex = index;
+        this.center = center;
+        this.bounds = bounds;
+    }
+}
