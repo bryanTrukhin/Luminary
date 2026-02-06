8b0decac858d7bbe5de1cefd93813d9e8fd347a7
Bryan Trukhin <bryantrukhin77@gmail.com>
2026-02-05 19:00:26 -0500

    Finished frog attacking and detection system

---
+++ Assets/TongueRendering.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TongueRendering : MonoBehaviour
+{
+    public GameObject toad;
+    public ToadAttacking toadAttacking;
+    public LineRenderer lineRenderer;
+    public CircleCollider2D circleCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toadAttacking = toad.GetComponent<ToadAttacking>();
+        circleCollider = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = toad.transform.position;
+        circleCollider.offset = transform.InverseTransformPoint(toadAttacking.tongueJoint.connectedAnchor);
+        lineRenderer.SetPosition(0, toadAttacking.transform.position);
+        lineRenderer.SetPosition(1, toadAttacking.tongueJoint.connectedAnchor);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("PLAYER WAS HIT");
+        }
+        if (collision.gameObject.CompareTag("Firefly"))
+        {
+            Debug.Log("FIREFLY WAS HIT");
+            toadAttacking.canExplode = true;
+        }
+    }
+}
